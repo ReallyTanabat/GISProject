df38752bb633001890f5d60ee486743ad9f2b5b9
ReallyTanabat <56994330+ReallyTanabat@users.noreply.github.com>
2022-11-23 01:47:16 +0700

    Done All step 5

--- GISProject/src/app/components/step5d/step5d.component.ts
+++ GISProject/src/app/components/step5d/step5d.component.ts
@@ -18,6 +18,7 @@ export class Step5dComponent implements OnInit {showPop: boolean = false
   lati: any[] = []
   longti: any[] = []
   positioning: any[] = []
+  showing: boolean = false
   
   // mapOptions: google.maps.MapOptions = {
   //   center: { lat: 38.9987208, lng: -77.2538699 },
@@ -30,13 +31,7 @@ export class Step5dComponent implements OnInit {showPop: boolean = false
   constructor(private http: HttpClient) { }
   
     ngOnInit(): void {
-      this.callAP()
-      console.log("current position 2 = ",this.positioning)
-      
-    }
-
-    callAP(){
-        this.http.get<any>(this.url+'/api/quest5/d').subscribe(data => {
+      this.http.get<any>(this.url+'/api/quest5/d').subscribe(data => {
         this.toShowData = data;
         this.object = data.data
         console.log("lat = ",this.object[1].latitude)
@@ -45,19 +40,20 @@ export class Step5dComponent implements OnInit {showPop: boolean = false
           //this.positioning.push({ lat: this.object[i].latitude, lng: this.object[i].longitude})
           this.positioning.push({ lat: this.object[i].longitude, lng: this.object[i].latitude})
         }
-        
         console.log("current position = ",this.positioning)
+        this.showing = true
         
-    })
+    })  
     }
+
+   
 
   center: google.maps.LatLngLiteral = {
       lat: 13.74622118,
       lng: 100.5544779
   };
   zoom = 4;
-  vertices: google.maps.LatLngLiteral[] = []
+  vertices: google.maps.LatLngLiteral[] = this.positioning
   checkFN(){
-    console.log(this.positioning)
   }
 }
--- GISProject/src/app/components/step5f1/step5f1.component.ts
+++ GISProject/src/app/components/step5f1/step5f1.component.ts
@@ -1,19 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-step5f1',
   templateUrl: './step5f1.component.html',
   styleUrls: ['./step5f1.component.css']
 })
-export class Step5f1Component implements OnInit {
-  constructor() { }
-  ngOnInit(): void {
-  }
-  mapOptions: google.maps.MapOptions = {
-    center: { lat: 38.9987208, lng: -77.2538699 },
-    zoom: 14
-  }
-  marker = {
-    position: { lat: 38.9987208, lng: -77.2538699 },
-  }
+export class Step5f1Component implements OnInit {showPop: boolean = false
+  color: string = ""
+  year: string = ""
+  dataCheck: any[] = [];
+  toShowData: any[] = []
+  toShow: boolean = false
+  object: any
+  url: string = "https://438d-1-20-61-94.ap.ngrok.io"
+  lati: any[] = []
+  longti: any[] = []
+  positioning: any[] = []
+  
+  // mapOptions: google.maps.MapOptions = {
+  //   center: { lat: 38.9987208, lng: -77.2538699 },
+  //   zoom: 14
+  // }
+  // marker = {
+  //   position:{lat: 38.9987208, lng: -77.2538699, }
+    
+  // }
+  constructor(private http: HttpClient) { }
+  
+    ngOnInit(): void {
+      if(this.positioning.length != 0){
+        this.positioning = []
+        this.markerPositions = []
+      }
+
+    }
+
+    callAP(){
+      if(this.positioning.length != 0){
+        this.positioning = []
+        this.markerPositions = []
+      }
+     
+        this.http.get<any>(this.url+'/api/quest5/f/'+this.year).subscribe(data => {
+        this.toShowData = data;
+        this.object = data.data
+        this.toShow = true
+        this.markerPositions = this.positioning
+        for(let i = 0; i<this.object.length;i++){
+          this.positioning.push({ lat: this.object[i].latitude, lng: this.object[i].longitude})
+        }
+        console.log("current position = ",this.positioning)
+        
+        })
+    
+    }      
+
+    @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow | undefined;
+    center: google.maps.LatLngLiteral = {
+        lat: 24,
+        lng: 12
+    };
+    markerPositions: google.maps.LatLngLiteral[] = this.positioning;
+    zoom = 4;
+    addMarker(event: google.maps.MapMouseEvent) {
+        if (event.latLng != null) this.markerPositions.push(event.latLng.toJSON());
+    }
+    openInfoWindow(marker: MapMarker) {
+        if (this.infoWindow != undefined) this.infoWindow.open(marker);
+    }
+
 }
